99952947efc4d23253e8a49fe880e73702fb5ef9
nomihla <84141282+nomihla@users.noreply.github.com>
2021-05-15 21:33:10 +0200

    Add files via upload

---
+++ nomihle.java
@@ -0,0 +1,13 @@
+/*Nomihle Cafuza
+  219007055
+  Java Assignment 2
+*/
+
+import javax.swing.*;
+public class nomihle {
+    public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Programming if fun",null, JOptionPane.INFORMATION_MESSAGE);
+     System.exit(0);
+
+    } 
+}
